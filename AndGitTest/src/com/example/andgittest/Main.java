01b6a1b66042d36e6dc9275107a66cc3c2c3f9a6
abc <abc@abc>
2016-09-14 10:56:10 +0900

    modify

--- AndGitTest/src/com/example/andgittest/Main.java
+++ AndGitTest/src/com/example/andgittest/Main.java
@@ -2,11 +2,15 @@ package com.example.andgittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		tv.setText("음음");
 	}
 }
